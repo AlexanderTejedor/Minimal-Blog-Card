3a974eb5c0fffa740fa3c05317198a129a00e7e1
Alexander Tejedor <atromero.98@gmail.com>
2025-05-24 10:49:13 -0500

    feat(deploy): Implement GH-Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://AlexanderTejedor.github.io/Minimal-Blog-Card'
 })
